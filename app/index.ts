97a9b1f4d6724900892575bc95493d4338dc632a
Sorayut Nookaew <sorayut-no@rmutp.ac.th>
2025-08-21 12:04:01 +0700

    new ts

--- app/index.ts
+++ app/index.ts
@@ -6,7 +6,7 @@ app.get("/", (c) => c.text("Hello, World!"));
 
 app.get("/about", (c) => {
     return c.json({
-        message: "About Page"
+        message: "Sorayut Nookaew"
     });
 });
 
--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,5 @@
 import { serve } from "@hono/node-server";
-import app from "./index";
+import app from "./index.js";
 
 serve(app, (info) => {
     console.log(`Server is running on ${info.port}`);
