7f4fa11917a43520bae5d0788293eedcc20e943a
Sorayut Nookaew <sorayut-no@rmutp.ac.th>
2025-09-03 17:25:04 +0700

    save to db

--- app/index.ts
+++ app/index.ts
@@ -31,10 +31,14 @@ app.post("/profile", async(c) => {
     body.password = passwordHash;
     console.log('body.password(replace ', body);
     //save to db
-
+    body.status= false;
+    const result = await prisma.profile.create({
+        data: body
+    });
     //output 
     return c.json({
-        message: "create profile completed"
+        message: "create profile completed",
+        data: result
     });
 });
 
